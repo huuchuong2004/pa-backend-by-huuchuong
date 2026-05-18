3ca125d95820fe4ae190b4821844b825237aedf7
huuchuong2004 <huuchuong398@gmail.com>
2026-05-18 17:29:34 +0700

    finish set up basiclly  the base for project

---
+++ src/main/java/vn/huuchuong/pabackbyhuuchuong/PaBackByHuuchuongApplication.java
@@ -0,0 +1,13 @@
+package vn.huuchuong.pabackbyhuuchuong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaBackByHuuchuongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaBackByHuuchuongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/huuchuong/pabackbyhuuchuong/PaBackByHuuchuongApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.huuchuong.pabackbyhuuchuong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaBackByHuuchuongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
